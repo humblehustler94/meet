d3b616eed5330bbe2a5daaf9edf8fff713893bae
humblehustler94 <flores.itzel94@gmail.com>
2025-07-15 16:45:41 -0700

    Red Step a new TDD cycle write a new failing test inside CitySearch.test.js that uses user-event
    Added two new tests for clarity
    1. One that checks that the suggestion list is hidden by default
    2. One that clicks the input and expects the list to become visible.
    3. Confirm the failure by run npm test :
    TestingLibraryElementError: Unable to find an accessible element with the role "list"
    Test Suites 1 Failed, 2 Passed , 3 total
    Tests 1 Failed, 6 Passed, 7 Total

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,19 +1,38 @@
 // src/__test__/CitySearch.test.js
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'; // <--- 1. IMPORT new code line added
 import CitySearch from '../components/CitySearch';
 
 describe('<CitySearch /> component', () => {
+    // Test 1 : Already exsists and passes
     test('renders text input', () => {
-        // 1. Render the component 
         render(<CitySearch />);
-
-        // 2. Find the input element by its role
-        // We use getByRole because we EXPECT it to be there.
         const cityTextBox = screen.getByRole('textbox');
-
-        // 3. Assert that the textbox is in the document AND has the correct class
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass('city');
+    });
+
+    // --- Test 2 New failing test to add ---
+    test('suggestions list is hidden by default', async () => {
+        render(<CitySearch />);
+        const suggestionList = screen.queryByRole('list');
+        expect(suggestionList).not.toBeInTheDocument();
+    });
+
+    test('renders a list of suggestions when city textbox is clicked', async () => {
+        // 1. set up user instance
+        const user = userEvent.setup();
+        render(<CitySearch />);
+
+        // 2. Find the textbox
+        const cityTextBox = screen.getByRole('textbox');
+
+        // 3. Simulate a click
+        await user.click(cityTextBox);
+
+        // 4. Assert that the suggestion list is now visible 
+        const suggestionList = screen.getByRole('list');
+        expect(suggestionList).toBeInTheDocument();
     });
 });
