03524693ff8249a4f3f7eee0305f947363c77441
humblehustler94 <flores.itzel94@gmail.com>
2025-07-15 12:46:35 -0700

    Red Step of a new TDD cycle. Writing a new failing test inside CitySearch.text.js
    Add userEvent import to the top of CitySearch.test.js file
    Add a new test( ) block inside the describe block.
    Added two new tests for clarity
    1. one checks that the suggestion list is hidden by default
    2. one that clicks the input and expects the list to become visible
    
    Confirm failure by run npm test : Results in error
    TestingLibraryElementError : Unable to find an accessible element with the role "list"

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,19 +1,30 @@
 // src/__test__/CitySearch.test.js
 
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'; // <-- 1. IMPORT new line added to file 
 import CitySearch from '../components/CitySearch';
 
 describe('<CitySearch /> component', () => {
-    test('renders text input', () => {
-        // 1. Render the component 
+    test('renders text input', () => { 
+        render(<CitySearch />); // 1.
+        const cityTextBox = screen.getByRole('textbox'); // 2.
+        expect(cityTextBox).toBeInTheDocument(); // 3.
+        expect(cityTextBox).toHaveClass('city');
+    });
+
+    test('renders a list of suggestions when city textbox is clicked', async () => {
+        // 1. Set up the user instance
+        const user = userEvent.setup();
         render(<CitySearch />);
 
-        // 2. Find the input element by its role
-        // We use getByRole because we EXPECT it to be there.
+        // 2. Find the textbox
         const cityTextBox = screen.getByRole('textbox');
 
-        // 3. Assert that the textbox is in the document AND has the correct class
-        expect(cityTextBox).toBeInTheDocument();
-        expect(cityTextBox).toHaveClass('city');
+        // 3. Simulate a click
+        await user.click(cityTextBox);
+
+        // 4. Assert that the suggestion list is now visible
+        const suggestionList = screen.getByRole('list');
+        expect(suggestionList).toBeInTheDocument();
     });
 });
