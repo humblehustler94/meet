494a81be2dcc33c645a9338c47fca7cf341e0f98
humblehustler94 <flores.itzel94@gmail.com>
2025-07-17 18:20:41 -0700

    Refactor (Event.jsx) for Green Step : To allow 4 tests to pass
    Refactor code in Event.jsx to allow the test to pass from the previous commit.

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,9 +2,14 @@
 
 // This component represents a single event in the list.
 // For now, it's just an empty list item.
-const Event =() => {
+const Event =( { event }) => {
     return (
-        <li></li>
+        <li>
+            <h2>{event.summary}</h2>
+            <p>{event.start.dateTime}</p>
+            <p>{event.location}</p>
+            <button>Show Details</button>
+        </li>
     );
 }
 
