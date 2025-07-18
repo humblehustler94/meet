ff667561f25ee4d4894f761f4d1bc11724487be0
humblehustler94 <flores.itzel94@gmail.com>
2025-07-17 18:31:30 -0700

    Add new test inside the describe block in src/__test___/Event.test.js
    Step 4: Write the next failing Tests (Interaction Tests) (Red Step)
    Now test the show/hide functionality.
    Add new test inside the describe block in src/__test___/Event.test.js

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,5 +1,6 @@
 // src/Event.test.js Step 1: Create a new file within src folder
 // Step 2: Write the first failing test (The Red Step)
+import userEvent from '@testing-library/user-event'; // Add import to the top of the list
 import { render, screen } from '@testing-library/react';
 import Event from '../components/Event';
 import mockData from '../mock-data';
@@ -27,4 +28,39 @@ describe('<Event /> component', () => {
         render(<Event event={testEvent} />);
         expect(screen.getByRole('button', { name: /show details/i })).toBeInTheDocument();
     });
+
+    test('by default, event details section should be hidden', () => {
+        render(<Event event={testEvent} />);
+        const details = screen.queryByText(testEvent.description);
+        expect(details).not.toBeInTheDocument();
+    });
+
+    test('shows the details section when the user clicks on the "show details" button', async () => {
+        const user = userEvent.setup();
+        render(<Event event={testEvent} />);
+
+        const showDetailsButton = screen.getByRole('button', { name: /show details/i });
+        await user.click(showDetailsButton);
+
+        const details = screen.getByText(testEvent.description);
+        expect(details).toBeInTheDocument();
+    });
+
+    test('hides the details section when the user clicks on the "hide details" button', async () => {
+        const user = userEvent.setup();
+        render(<Event event={testEvent} />);
+
+        // First, click to show details
+        const showDetailsButton = screen.getByRole('button', { name: /show details/i });
+        await user.click(showDetailsButton);
+
+        // Then, click to hide details
+        const hideDetailsButton = screen.getByRole('button', { name: /hide details/i });
+        await user.click(hideDetailsButton);
+
+        const details = screen.queryByText(testEvent.description);
+        expect(details).not.toBeInTheDocument();
+
+    });
+
 });
