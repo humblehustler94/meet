f88b0f0606f550c25d6242703b827fe36cfb739f
humblehustler94 <flores.itzel94@gmail.com>
2025-08-04 17:30:52 -0700

    chore: Remove Atatus test error after verification
    Successfully Integrated Atatus to project.
    Deleted code lines 10 and 21 - 25

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import { getEvents, extractLocations } from './api';
 
-import * as atatus from 'atatus-spa'; // <-- 1. IMPORT ATATUS
 
 function App() {
   // All authentication-related state and functions have been removed.
@@ -18,13 +17,7 @@ function App() {
 
   // This useEffect now only focuses on fetching data.
   useEffect(() => {
-    // --- 2. ADD THIS TEST CODE ---
-    // THIS SENDS A TEST ERROR TO YOUR ATATUS DASHBOARD TO VERIFY SETUP.
-    atatus.notify(new Error('Test Atatus Setup - It works!'));
-    // --- END OF TEST CODE ---
-
-
-
+ 
     const fetchData = async () => {
       const allEvents = await getEvents();
       setAllLocations(extractLocations(allEvents));
